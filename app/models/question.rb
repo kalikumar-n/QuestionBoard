3a8ac7c6fef35f79b412c9b7cafec351fd974c64
kalikumar-n <kalikumar1701@gmail.com>
2023-06-02 17:35:54 +0530

    Added Validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
+  validates_presence_of :title
+  validates_presence_of :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_secure_password
-
+  validates :email, presence: :true, uniqueness: :true
+  validates_presence_of :name
   scope :active, -> {where(active: true)}
 end
